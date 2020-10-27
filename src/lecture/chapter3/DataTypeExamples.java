0891fdc6f3aad2c28d8a37b1206d6baf4f099f70
Matthias Berg-Neels <matthias.uwe.berg-neels@sap.com>
2020-10-27 18:32:23 +0100

    Data types and literal examples

---
+++ src/lecture/chapter3/DataTypeExamples.java
@@ -0,0 +1,30 @@
+package lecture.chapter3;
+
+public class DataTypeExamples {
+
+    public static void main(String[] args) {
+
+        // Deklaration einer boolean Variable
+        boolean isTired;
+
+        // Initialisierung der Variable isTired auf "true"
+        isTired = true; // true / false
+
+        // Deklaration und Initialisierung der Ganzzahlen Variable "numberA"
+        int numberA = 5;
+        double numberB = 3.41;
+
+        numberA = 98;
+
+        // Deklaration & Initialisierung der Konstanten PI
+        final double PI = 3.14;
+
+        // Ändern von Konstanten nicht möglich
+        // PI = 5.41;
+
+        // Ausgabe von numberA und PI
+        System.out.println(numberA);
+        System.out.println(PI);
+
+    }
+}
---
+++ src/lecture/chapter3/LiteralExamples.java
@@ -0,0 +1,69 @@
+package lecture.chapter3;
+
+public class LiteralExamples {
+
+    public static void main(String[] args) {
+
+        int numberA;
+
+        // Ganzzahlen Literale
+
+        // Dezimalliteral --> Zahlen 0 - 9
+        numberA = 153;
+        System.out.println(numberA); // Ausgabe: 153
+
+        // Oktalliteral --> präfix 0 Zahlen 0-7
+        numberA = 071;
+        System.out.println(numberA); // Ausgabe: 57 (7*8^1+1*8^0)
+
+        // Hexadezimalliteral --> präfix 0x Zahlen 0-9 & Buchstaben A-F
+        numberA = 0x1A;
+        System.out.println(numberA); // Ausgabe: 26 (1*16^1+10*16^0)
+
+        // Binärliteral --> präfix 0b Zahlen 0 & 1
+        numberA = 0b11;
+        System.out.println(numberA); // Ausgabe: 3
+
+        // Long Literal --> größer als Integer-Wertebereich
+        long largeNumberB = 3000000000L;
+
+
+        // Gleitkomma-Literal
+
+        double numberC;
+
+        numberC = .5; // 0.5
+        System.out.println(numberC); // Ausgabe: 0.5
+
+        numberC = 45.153e2d;
+        System.out.println(numberC); // Ausgabe: 4515.3
+
+        numberC = 45.153e-2d;
+        System.out.println(numberC); // Ausgabe: 0.45153
+
+        float numberD;
+
+        // float literal --> Suffix "f"
+        numberD = 3.14e2f;
+
+
+        // Alphanumerische Literale
+
+        char singleCharacter;
+
+        singleCharacter = 'v';
+        System.out.println(singleCharacter); // Ausgabe: v
+
+        singleCharacter = '\u2764';
+        System.out.println(singleCharacter); // Ausgabe: ❤
+
+        singleCharacter = 65; // ! Dezimalliteral
+        System.out.println(singleCharacter); // Ausgabe: A
+
+
+        String multipleCharacters;
+
+        multipleCharacters = "Hier steht \t ein \"Text\" aus\nvielen Zeichen! \u2764";
+        System.out.println(multipleCharacters); // Ausgabe: Hier steht ein "Text" aus vielen Zeichen!
+    }
+}
