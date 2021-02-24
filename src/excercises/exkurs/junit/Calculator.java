c9bd414edf825515151c9aafdea77584341e4f34
Matthias Berg-Neels <matthias.uwe.berg-neels@sap.com>
2021-02-24 18:53:54 +0100

    JUnit Calculator example

---
+++ src/excercises/exkurs/junit/Calculator.java
@@ -0,0 +1,13 @@
+package excercises.exkurs.junit;
+
+public class Calculator {
+
+    public double add(double numberA, double numberB){
+        return numberA + numberB;
+    }
+
+    public double substract(double numberA, double numberB){
+        return numberA - numberB;
+    }
+
+}
