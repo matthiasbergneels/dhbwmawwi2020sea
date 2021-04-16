08ba4ecf7ad84baeea47b01f7e27b1bc60d11a6a
Matthias Berg-Neels <matthias.uwe.berg-neels@sap.com>
2021-04-16 15:08:25 +0200

    Stack

---
+++ src/lecture/chapter12/Stack.java
@@ -0,0 +1,49 @@
+package lecture.chapter12;
+
+public class Stack<D> {
+
+    Node<D> topNode = null;
+
+    // lege ein Element auf den Stack
+    public void push(D data){
+        topNode = new Node(data, topNode);
+    }
+
+    // nehme das oberste Element vom Stack runter
+    public D pop(){
+        if(topNode != null){
+            D data = topNode.getData();
+            topNode = topNode.getLowerNode();
+            return data;
+        }
+        return null;
+    }
+
+    // zeige das oberste Element vom Stack
+    public D peek(){
+        if(topNode != null){
+            return topNode.getData();
+        }
+        return null;
+    }
+
+
+    private class Node<D>{
+
+        private D data;
+        private Node<D> lowerNode;
+
+        public Node(D data, Node<D> lowerNode){
+            this.data = data;
+            this.lowerNode = lowerNode;
+        }
+
+        public Node<D> getLowerNode(){
+            return this.lowerNode;
+        }
+
+        public D getData(){
+            return this.data;
+        }
+    }
+}
