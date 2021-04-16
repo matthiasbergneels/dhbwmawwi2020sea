cc1ede072e497682d4ea509e4076997563f14967
Matthias Berg-Neels <matthias.uwe.berg-neels@sap.com>
2021-04-16 11:39:41 +0200

    LinkedList: recursiveContains & remove

--- src/lecture/chapter12/LinkedList.java
+++ src/lecture/chapter12/LinkedList.java
@@ -44,8 +44,68 @@ public class LinkedList {
     }
 
     // contains --> rekursiv
+    public boolean recursiveContains(Object data){
+        if(firstNode != null){
+            return recursiveContains(data, firstNode);
+        }
+
+        return false;
+    }
+
+    private boolean recursiveContains(Object data, Node currentNode){
+        if(currentNode.getData().equals(data)){
+            return true;
+        }
+
+        if(currentNode.getNextNode() != null){
+            return recursiveContains(data, currentNode.getNextNode());
+        }
+
+        return false;
+    }
 
     // remove Methode: public boolean remove(Object data)
+    public boolean remove(Object data){
+
+        if(firstNode != null){
+
+            if(firstNode.getData().equals(data)){
+                firstNode = firstNode.getNextNode();
+                return true;
+            }
+
+            Node currentNode = firstNode;
+            while(currentNode.getNextNode() != null){
+                if(currentNode.getNextNode().getData().equals(data)){
+                    currentNode.setNextNode(currentNode.getNextNode().getNextNode());
+                    return true;
+                }
+                currentNode = currentNode.getNextNode();
+            }
+        }
+
+        return false;
+    }
+
+    public void printList(){
+        if(firstNode == null){
+            System.out.println("Liste ist leer");
+            return;
+        }
+
+        System.out.println("Liste:");
+        printList(firstNode);
+
+    }
+
+    private void printList(Node currentNode){
+        System.out.println("- " + currentNode.data);
+        if(currentNode.getNextNode() != null){
+            printList(currentNode.getNextNode());
+        }else{
+            System.out.println("Ende der Liste");
+        }
+    }
 
     private class Node{
 
--- src/lecture/chapter12/LinkedListExample.java
+++ src/lecture/chapter12/LinkedListExample.java
@@ -12,9 +12,29 @@ public class LinkedListExample {
 
         System.out.println("Liste gefüllt!");
 
+        System.out.println("Contains - Iterarativ");
         System.out.println("Enthält Hallo: " + myWordList.contains("Hallo"));
         System.out.println("Enthält Klaus: " + myWordList.contains("Klaus"));
         System.out.println("Enthält Hihihi: " + myWordList.contains("Hihihi"));
 
+        System.out.println("Contains - Rekursiv");
+        System.out.println("Enthält Hallo: " + myWordList.recursiveContains("Hallo"));
+        System.out.println("Enthält Klaus: " + myWordList.recursiveContains("Klaus"));
+        System.out.println("Enthält Hihihi: " + myWordList.recursiveContains("Hihihi"));
+
+        myWordList.printList();
+
+        System.out.println("Lösche Klaus: " + myWordList.remove("Klaus"));
+
+        myWordList.printList();
+
+        System.out.println("Lösche Hallo: " + myWordList.remove("Hallo"));
+        System.out.println("Lösche Hallo: " + myWordList.remove("Hallo"));
+
+        myWordList.printList();
+
+        System.out.println("Lösche Hihihi: " + myWordList.remove("Hihihi"));
+
+        myWordList.printList();
     }
 }
