1db949b38fdd5a16e5a844c140dab088bae352db
Matthias Berg-Neels <matthias.uwe.berg-neels@sap.com>
2021-04-21 18:06:11 +0200

    Selection Sort

--- src/lecture/chapter13/SortAlgorithms.java
+++ src/lecture/chapter13/SortAlgorithms.java
@@ -6,45 +6,56 @@ public class SortAlgorithms {
         int[] unsortedNumbers = {50, 30, 80, 40, 60, 100, 20, 70, 10, 90};
 
         int[] sortedNumbers = bubbleSortNumberArrayV1(unsortedNumbers.clone());
+        System.out.println("Sortierte Zahlen - bubbleSortV1: ");
+        printArray(sortedNumbers);
 
-        System.out.println("Sortierte Zahlen - bubbleSort: ");
-        for(int number : sortedNumbers){
-            System.out.print(number + "; ");
-        }
+
+        sortedNumbers = bubbleSortNumberArrayV1(unsortedNumbers.clone());
+        System.out.println("Sortierte Zahlen - bubbleSortV2: ");
+        printArray(sortedNumbers);
+
+
+        sortedNumbers = bubbleSortNumberArrayV1(unsortedNumbers.clone());
+        System.out.println("Sortierte Zahlen - bubbleSortV3: ");
+        printArray(sortedNumbers);
+
+
     }
 
     public static int[] bubbleSortNumberArrayV1(int numbers[]){
 
+        long startTime = System.currentTimeMillis();
+
         for (int i = 0; i < numbers.length; i++){
-            System.out.println("- Äußere Schleife Durchlauf " + i);
             for(int j = 0; j < (numbers.length - 1); j++){
-                System.out.println("-- Innere Schleife Durchlauf " + i + "-" + j);
                 if(numbers[j] > numbers[j+1]){
-                    int cache = numbers[j];
-                    numbers[j] = numbers[j+1];
-                    numbers[j+1] = cache;
-                    System.out.println("--- Getauscht! " + i + "-" + j);
+                    swap(numbers, j, j+1);
                 }
             }
         }
+
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("BubbleSort V1 Runtime: " + (endTime - startTime) + " ms");
 
         return numbers;
     }
 
     public static int[] bubbleSortNumberArrayV2(int numbers[]){
 
+        long startTime = System.currentTimeMillis();
+
         for (int i = 0; i < numbers.length; i++){
-            System.out.println("- Äußere Schleife Durchlauf " + i);
             for(int j = 0; j < (numbers.length - 1 - i); j++){
-                System.out.println("-- Innere Schleife Durchlauf " + i + "-" + j);
                 if(numbers[j] > numbers[j+1]){
-                    int cache = numbers[j];
-                    numbers[j] = numbers[j+1];
-                    numbers[j+1] = cache;
-                    System.out.println("--- Getauscht! " + i + "-" + j);
+                    swap(numbers, j, j+1);
                 }
             }
         }
+
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("BubbleSort V2 Runtime: " + (endTime - startTime) + " ms");
 
         return numbers;
     }
@@ -52,7 +63,48 @@ public class SortAlgorithms {
 
     public static int[] bubbleSortNumberArrayV3(int numbers[]){
 
+        int j = numbers.length - 1;
+        boolean swapped = false;
+
+        long startTime = System.currentTimeMillis();
+
+        do{
+            swapped = false;
+
+            for(int i = 0; i < j; i++){
+                if(numbers[i] > numbers[i+1]){
+                    swap(numbers, i, i+1);
+                    swapped = true;
+                }
+            }
+            j--;
+        }while(swapped);
+
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("BubbleSort V3 Runtime: " + (endTime - startTime) + " ms");
 
         return numbers;
     }
+
+    public static int[] selectionSortNumberArray(int numbers[]){
+
+
+        return numbers;
+    }
+
+    private static void swap(int[] array, int a, int b) {
+        int temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
+    }
+
+    private static void printArray(int[] array){
+        System.out.print("Array-Ausgabe: ");
+        for(int i : array){
+            System.out.print(i + ", ");
+        }
+        System.out.println();
+    }
 }
+
---
+++ src/test/lecture/chapter13/SortAlgorithmsTest.java
@@ -0,0 +1,77 @@
+package test.lecture.chapter13;
+
+import lecture.chapter13.SortAlgorithms;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SortAlgorithmsTest {
+
+    static int count = 10000;
+    static int randomRange = 3000000;
+    static int[] toSort = null;
+
+    @BeforeEach
+    void setUp() {
+        generateArray();
+    }
+
+    @Test
+    void bubbleSortNumberArrayV1() {
+        int[] bubbleSortedArray = SortAlgorithms.bubbleSortNumberArrayV1(toSort.clone());
+        assertTrue(isSorted(bubbleSortedArray));
+    }
+
+    @Test
+    void bubbleSortNumberArrayV2() {
+        int[] bubbleSortedArray = SortAlgorithms.bubbleSortNumberArrayV2(toSort.clone());
+        assertTrue(isSorted(bubbleSortedArray));
+    }
+
+    @Test
+    void bubbleSortNumberArrayV3() {
+        int[] bubbleSortedArray = SortAlgorithms.bubbleSortNumberArrayV3(toSort.clone());
+        assertTrue(isSorted(bubbleSortedArray));
+    }
+
+    @Test
+    void selectionSortNumberArray() {
+        int[] selectionSortedArray = SortAlgorithms.selectionSortNumberArray(toSort.clone());
+        assertTrue(isSorted(selectionSortedArray));
+    }
+
+    private static boolean isSorted(int[] array){
+        for(int i = 0; i < array.length-1; i++){
+            if(array[i] > array[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    private static void generateArray(){
+        toSort = new int[count];
+        for(int i = 0; i < count; i++){
+            boolean notInserted = true;
+            while(notInserted){
+                int randomNumber = (int)(Math.random() * randomRange);
+
+                boolean found = false;
+                for(int j = 0; j < i; j++){
+                    if(toSort[j] == randomNumber){
+                        found = true;
+                        break;
+                    }
+                }
+
+                if(!found){
+                    toSort[i] = randomNumber;
+                    notInserted = false;
+                }
+
+            }
+        }
+    }
+}
